b30849981ce328b9df608c9c79bce6afda414b24
Sakshi80506 <sakshikamble0506@gmail.com>
2025-02-13 21:46:23 +0530

    Create about.js

---
+++ about.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <section id="about" className="about">
+      <div className="about-content">
+        <h2>About Me</h2>
+        <p>
+          I'm a passionate web developer with experience in front-end technologies.
+        </p>
+        <p>
+          I love building intuitive, dynamic, and responsive websites. My skill set includes HTML, CSS, JavaScript, React, and more.
+        </p>
+        <p>
+          I’m always eager to learn new technologies and grow as a developer. I have attended the IDEAS College Project Competition and reached the finals, and also participated in the NIT Hackathon , where I had the opportunity to collaborate with talented individuals.
+        </p>
+        <p>
+          I'm committed to continuously improving my skills and keeping up with the latest trends in web development.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
